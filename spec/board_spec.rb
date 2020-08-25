4b0a662b8803bdfec42cec7f9365fae756977dbd
Samuel <samuelalmaoherrera@gmail.com>
2020-08-25 16:17:34 -0300

    Fixed linter errors.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,39 +1,36 @@
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 describe Board do
-  let (:board) {Board.new}
-  let (:player1) {Player.new('Mercedes', 'X')}
-  let (:array_win) {[1, 2, 3]}
-  let (:array_loser) {[1, 3, 7]}
-  let (:arr_player) {}
+  let(:board) { Board.new }
+  let(:player1) { Player.new('Mercedes', 'X') }
+  let(:array_win) { [1, 2, 3] }
+  let(:array_loser) { [1, 3, 7] }
+  let(:arr_player) {}
 
-
-
-  describe "#create_board" do
-    it "returns board" do
+  describe '#create_board' do
+    it 'returns board' do
       expect(board.create_board).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
-  describe "#validate_symbol" do
-    context "Check if the symbols valid" do
+  describe '#validate_symbol' do
+    context 'Check if the symbols valid' do
       it "Check if the symbol is 'X' or 'O'" do
         expect(board.validate_symbol('X')).to be(true)
       end
-      it "Returns an exception for an invalid input" do
+      it 'Returns an exception for an invalid input' do
         expect(board.validate_symbol(2)).to be(false)
       end
     end
   end
 
-  describe "#validate_turn" do
-  
+  describe '#validate_turn' do
     context 'Check if position is valid' do
       it 'return false is position is out of boundaries' do
         board.create_board
         expect(board.validate_turn(11)).to be(false)
       end
-      it 'return false if position is alredy taken' do 
+      it 'return false if position is alredy taken' do
         board.create_board
         board.place_symbol(1, player1)
         expect(board.validate_turn(1)).to be(false)
@@ -52,27 +49,27 @@ describe Board do
 
   describe '#check_win' do
     it 'Returns true when there is a winner' do
-      expect(board.check_win(array_win)).to be(true)      
+      expect(board.check_win(array_win)).to be(true)
     end
 
     it 'Returns true when there is not a winner' do
-      expect(board.check_win(array_loser)).to be(false)      
+      expect(board.check_win(array_loser)).to be(false)
     end
   end
 
   describe '#winner?' do
-    it "returns true if a player wins" do
-      for i in (1..3) do
+    it 'returns true if a player wins' do
+      (1..3).each do |i|
         player1.array = i
       end
       expect(board.winner?(player1)).to be(true)
     end
 
-    it "returns false if a player loses" do
-      for i in (2..4) do
+    it 'returns false if a player loses' do
+      (2..4).each do |i|
         player1.array = i
       end
       expect(board.winner?(player1)).to be(false)
-    end    
+    end
   end
 end
