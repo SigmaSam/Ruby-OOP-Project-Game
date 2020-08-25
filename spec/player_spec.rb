28a7bd5ecde2a48719ed5763a0cb53d3c234c460
Samuel <samuelalmaoherrera@gmail.com>
2020-08-25 16:20:51 -0300

    Fixed conflics.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,27 +1,31 @@
 require_relative '../lib/player.rb'
 
+
+
 describe Player do
-  let(:player1) { Player.new('Mercedes', 'X') }
+let(:player1) {Player.new('Mercedes', 'X')}
 
-  it 'has the symbol and name attributes' do
-    expect(player1).to have_attributes(name: 'Mercedes', symbol: 'X')
-  end
-  describe '#score_up' do
-    it 'increment score varible by 1' do
-      score = player1.score
-      expect(player1.score_up).to be(score + 1)
-    end
-  end
+it 'has the symbol and name attributes' do
+  expect(player1).to have_attributes(name: 'Mercedes', symbol: 'X')
+end
 
-  describe '#array' do
-    it 'push a new number into player array' do
-      expect(player1.array = 1).to eql(player1.array.length)
+    describe '#score_up' do
+        it 'increment score varible by 1' do
+            score = player1.score
+            expect(player1.score_up).to be(score + 1)
+        end
     end
-  end
 
-  describe '#clear_array' do
-    it 'empty array' do
-      expect(player1.clear_array).to eql([])
+    describe '#array' do
+        it 'push a new number into player array' do
+            expect(player1.array = 1).to eql(player1.array.length)
+        end
     end
-  end
+
+    describe '#clear_array' do 
+        it 'empty array' do
+            expect(player1.clear_array).to eql([])
+        end
+    end
+
 end
