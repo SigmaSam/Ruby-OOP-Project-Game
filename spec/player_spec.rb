83984407eda6091f2d6cc9a2418ffbe0837c4ce0
victorG <peter200970@hotmail.com>
2020-08-25 17:04:34 -0500

    tested player methods with rspec

---
+++ spec/player_spec.rb
@@ -0,0 +1,26 @@
+require_relative '../lib/player.rb'
+
+
+
+describe Player do
+let(:player1) {Player.new('Mercedes', 'X')}
+    describe '#score_up' do
+        it 'increment score varible by 1' do
+            score = player1.score
+            expect(player1.score_up).to be(score + 1)
+        end
+    end
+
+    describe '#array' do
+        it 'push a new number into player array' do
+            expect(player1.array = 1).to eql(player1.array.length)
+        end
+    end
+
+    describe '#clear_array' do 
+        it 'empty array' do
+            expect(player1.clear_array).to eql([])
+        end
+    end
+
+end
